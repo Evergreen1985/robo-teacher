124d457092de646cb460a9a80ce076ff39d106d0
Evergreen1985 <gowrisainathreddy@gmail.com>
2025-07-22 21:57:14 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-
 from fastapi import FastAPI, Request, UploadFile, File
-from fastapi.responses import JSONResponse, FileResponse, HTMLResponse
+from fastapi.responses import JSONResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import speech_recognition as sr
@@ -25,19 +24,16 @@ async def mic_upload(file: UploadFile = File(...)):
     with open(temp_input_path, "wb") as f:
         f.write(await file.read())
 
-    # Speech Recognition
     recognizer = sr.Recognizer()
     with sr.AudioFile(temp_input_path) as source:
         audio = recognizer.record(source)
         try:
             text = recognizer.recognize_google(audio)
-        except Exception as e:
+        except Exception:
             text = "Sorry, I didn't catch that."
 
-    # Convert response to voice
     tts = gTTS(text=text)
     tts.save(temp_output_path)
-
     os.remove(temp_input_path)
 
     return JSONResponse(content={
